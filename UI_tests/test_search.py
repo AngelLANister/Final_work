0ea66880027e3b61513b4be53e27416e796fa857
AngelLANister <bezrukandrej2@gmail.com>
2026-03-19 23:33:17 +0700

    первый тест

--- UI_tests/test_search.py
+++ UI_tests/test_search.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def test_search():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--incognito")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.get("https://www.kinopoisk.ru/?utm_referrer=www.kinopoisk.ru")
-    waiter = WebDriverWait(driver, 40)
-    waiter.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#js-button")))
-    driver.find_element(By.CSS_SELECTOR, "#js-button").click()
     waiter1 = WebDriverWait(driver, 40)
     waiter1.until(EC.visibility_of_element_located(
         (By.CSS_SELECTOR, 'button[class="styles_root__mwAP6"]')))
@@ -19,11 +19,10 @@ def test_search():
 
     driver.find_element(By.CSS_SELECTOR,
                       'svg[class="styles_icon__a6f9D search-form-submit-button__icon"]').click()
-
     name = driver.find_element(By.CSS_SELECTOR, 'div[class="element most_wanted"]')
     info = name.find_element(By.CSS_SELECTOR, 'div[class="info"]')
-    a = info.find_element(By.CSS_SELECTOR, 'a[data-type="film"]').text
+    film = info.find_element(By.CSS_SELECTOR, 'a[data-type="film"]').text
 
-    assert a == "Холоп"
+    assert film == "Холоп"
 
     driver.quit()
